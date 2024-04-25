76ccbaac73ccc45e5329c1175686704dc8dc891e
Rahul Kumar <rk8717989@gmail.com>
2024-04-25 17:05:10 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    $(".category").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1536,
+                settings: {
+                    slidesToShow: 5,
+                },
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+
+        ],
+    });
+    $(".schools-location").slick({
+        autoplay: false,
+        dots: false,
+        arrows: true,
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1536,
+                settings: {
+                    slidesToShow: 10,
+                },
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 8,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+
+        ],
+    });
+});
