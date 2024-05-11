f2d1a83d6546245c6947e99eb38b8539d6832461
Rahul Kumar <rk8717989@gmail.com>
2024-05-11 14:56:47 +0530

    blog detail add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -200,5 +200,39 @@ $(document).ready(function () {
 
         ],
     });
+    $(".bloglikemaybe").slick({
+        autoplay: true,
+        dots: false,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+
+        ],
+    });
 });
 
